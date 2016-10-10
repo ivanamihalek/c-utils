0b20c3b5063aa74ba26d79c878760d8c2d339d41
Ivana <ivana.mihalek@gmail.com>
2016-10-10 00:45:30 -0400

    daddy

--- ipc/04_threaded_socket/server.c
+++ ipc/04_threaded_socket/server.c
@@ -29,7 +29,7 @@ int main(int argc , char *argv[])
      
     //Prepare the sockaddr_in structure
     server.sin_family = AF_INET;
-     server.sin_addr.s_addr = INADDR_ANY;
+    server.sin_addr.s_addr = INADDR_ANY;
     server.sin_port = htons( 8888 );
      
     //Bind
@@ -76,6 +76,45 @@ int main(int argc , char *argv[])
         perror("accept failed");
         return 1;
     }
+     
+    return 0;
+}
+/*
+ * This will handle connection for each client
+ * */
+void *connection_handler(void *socket_desc)
+{
+    //Get the socket descriptor
+    int sock = *(int*)socket_desc;
+    int read_size;
+    char *message , client_message[2000];
+     
+    //Send some messages to the client
+    message = "Greetings! I am your connection handler\n";
+    write(sock , message , strlen(message));
+     
+    message = "Now type something and i shall repeat what you type \n";
+    write(sock , message , strlen(message));
+     
+    //Receive a message from client
+    while( (read_size = recv(sock , client_message , 2000 , 0)) > 0 )
+    {
+        //Send the message back to client
+        write(sock , client_message , strlen(client_message));
+    }
+     
+    if(read_size == 0)
+    {
+        puts("Client disconnected");
+        fflush(stdout);
+    }
+    else if(read_size == -1)
+    {
+        perror("recv failed");
+    }
+         
+    //Free the socket pointer
+    free(socket_desc);
      
     return 0;
 }
