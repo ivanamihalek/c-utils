67d9d4f39ccb7a38aee018a7b6ec8422bd23c945
Ivana Mihalek <ivana.mihalek@gmail.com>
2016-10-19 15:18:50 -0400

    componenets

--- igraph/autonomous_pieces/neighbors.c
+++ igraph/autonomous_pieces/neighbors.c
@@ -11,6 +11,86 @@ FILE * efopen(char * name, char * mode) {
     }
     return fp;
 }
+
+
+int print_attributes(const igraph_t *g) {
+
+    igraph_vector_t gtypes, vtypes, etypes;
+    igraph_strvector_t gnames, vnames, enames;
+    long int i;
+
+    igraph_vector_t vec;
+    igraph_strvector_t svec;
+    long int j;
+
+    igraph_vector_init(&gtypes, 0);
+    igraph_vector_init(&vtypes, 0);
+    igraph_vector_init(&etypes, 0);
+    igraph_strvector_init(&gnames, 0);
+    igraph_strvector_init(&vnames, 0);
+    igraph_strvector_init(&enames, 0);
+
+    igraph_cattribute_list(g, &gnames, &gtypes, &vnames, &vtypes, 
+			   &enames, &etypes);
+
+    /* vertex attributes */
+    for (i=0; i<igraph_vcount(g); i++) {
+	long int j;
+	printf("Vertex %li: ", i);
+	for (j=0; j<igraph_strvector_size(&vnames); j++) {
+	    printf("%s=", STR(vnames, j));
+	    if (VECTOR(vtypes)[j]==IGRAPH_ATTRIBUTE_NUMERIC) {
+		igraph_real_printf(VAN(g, STR(vnames,j), i));
+		putchar(' ');
+	    } else {
+		printf("\"%s\" ", VAS(g, STR(vnames,j), i));
+	    }
+	}
+	printf("\n");
+    }
+
+
+
+    /* Check vector-based query functions 
+       igraph_vector_init(&vec, 0);
+       igraph_strvector_init(&svec, 0);
+  
+       for (j=0; j<igraph_strvector_size(&vnames); j++) {
+       if (VECTOR(vtypes)[j]==IGRAPH_ATTRIBUTE_NUMERIC) {
+       igraph_cattribute_VANV(g, STR(vnames, j), igraph_vss_all(), &vec);
+       for (i=0; i<igraph_vcount(g); i++) {
+       igraph_real_t num=VAN(g, STR(vnames, j), i);
+       if (num != VECTOR(vec)[i] &&
+       (!isnan(num) || !isnan(VECTOR(vec)[i]))) {
+       exit(51);
+       }
+       }
+       } else {
+       igraph_cattribute_VASV(g, STR(vnames, j), igraph_vss_all(), &svec);
+       for (i=0; i<igraph_vcount(g); i++) {
+       const char *str=VAS(g, STR(vnames, j), i);
+       if (strcmp(str,STR(svec, i))) {
+       exit(52);
+       }
+       }
+       }
+       }
+    */
+ 
+
+    igraph_strvector_destroy(&svec);
+    igraph_vector_destroy(&vec);
+
+    igraph_strvector_destroy(&enames);
+    igraph_strvector_destroy(&vnames);
+    igraph_strvector_destroy(&gnames);
+    igraph_vector_destroy(&etypes);
+    igraph_vector_destroy(&vtypes);
+    igraph_vector_destroy(&gtypes);
+
+    return 0;
+}
+
 /************************************************/
 long int find_vector_by_name(igraph_t * g){
     /*** do we have the name attribute ? ***/
@@ -70,19 +150,8 @@ int main(int argc, char*argv[]) {
 	fprintf (stderr, "Error reading %s\n", argv[1]);
     }
     printf ("number of vertices:  %d\n",  igraph_vcount(&graph));
-    /* setup iterator over all vertices: selector */
-    igraph_vs_t all_vertices_selector;
-    igraph_vit_t vertex_iterator;
-    igraph_vs_all  (&all_vertices_selector);
-    igraph_integer_t size;
-    igraph_vs_size (&graph, &all_vertices_selector, &size);
-    printf ("iterator size: %li \n", (long int) size);
-
-     igraph_vit_create(&graph, all_vertices_selector, &vertex_iterator);
-     while (!IGRAPH_VIT_END(vertex_iterator)) {
-         printf("\t %li \n", (long int) IGRAPH_VIT_GET(vertex_iterator));
-         IGRAPH_VIT_NEXT(vertex_iterator);
-     }
+  
+    print_attributes (&graph);
     
     //igraph_vit_create (&graph, vertices, &vertex_iterator);
     
@@ -113,7 +182,6 @@ int main(int argc, char*argv[]) {
 	print_vector(VECTOR(result)[i]);
     }
     
-    igraph_vit_destroy (&vertex_iterator);
     igraph_vector_ptr_destroy(&result);
     igraph_destroy(&graph);
 
