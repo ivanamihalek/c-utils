0bc8f8826261a63af85d0133123952e7bc00de3e
Ivana Mihalek <ivana.mihalek@gmail.com>
2016-10-19 09:30:36 -0400

    componenets

--- igraph/autonomous_pieces/components.c
+++ igraph/autonomous_pieces/components.c
@@ -38,9 +38,26 @@ int main(int argc, char*argv[]) {
     
     igraph_bool_t directed  = IGRAPH_UNDIRECTED;
     igraph_read_graph_edgelist (&graph, infile,  zero, directed);
-    int number_of_components  =  count_components(graph, "");
+
+    /*int igraph_decompose(const igraph_t *graph, igraph_vector_ptr_t *components, 
+		     igraph_connectedness_t mode,
+		     long int maxcompno, long int minelements);
+    */
+    igraph_vector_ptr_t components;
+    igraph_vector_ptr_init (&components, 0);
+
+    igraph_decompose (&graph, &components, IGRAPH_WEAK, -1, 2);
+    
+    int number_of_components  =  igraph_vector_ptr_size(&components);
+    
     printf ("number of components:  %d\n", number_of_components);
- 
+
+    /*Don't forget to call igraph_destroy() and free() on the elements of
+      this pointer vector to free unneeded memory. Alternatively, you can simply
+      call igraph_decompose_destroy() that does this for you. 
+    */
+    
+    igraph_vector_ptr_destroy(&components);
     igraph_destroy(&graph);
 
     if (!IGRAPH_FINALLY_STACK_EMPTY) return 1;
