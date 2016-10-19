a3fa17bb15efa7ba46fbce7aa5cff297e4d9a4b4
Ivana Mihalek <ivana.mihalek@gmail.com>
2016-10-19 12:57:45 -0400

    componenets

--- igraph/autonomous_pieces/neighbors.c
+++ igraph/autonomous_pieces/neighbors.c
@@ -15,8 +15,8 @@ FILE * efopen(char * name, char * mode) {
 long int find_vector_by_name(igraph_t * g){
     /*** do we have the name attribute ? ***/
     printf ("have names?  %d\n",
-	    (int) igraph_cattribute_has_attr (g, IGRAPH_ATTRIBUTE_VERTEX, "vnames"));
-    
+	    (int) igraph_cattribute_has_attr (g, IGRAPH_ATTRIBUTE_VERTEX, "name"));
+    return 0;
 }
 
 
